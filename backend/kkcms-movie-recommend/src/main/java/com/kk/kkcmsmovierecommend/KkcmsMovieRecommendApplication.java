1dc866527dcf3f7a06c09508434b1d7533782ecc
hutwanghui <zjjhwanghui@163.com>
2018-11-03 19:11:52 +0800

    新增电影推荐系统

---
+++ backend/kkcms-movie-recommend/src/main/java/com/kk/kkcmsmovierecommend/KkcmsMovieRecommendApplication.java
@@ -0,0 +1,16 @@
+package com.kk.kkcmsmovierecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableKafka
+public class KkcmsMovieRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkcmsMovieRecommendApplication.class, args);
+    }
+}
